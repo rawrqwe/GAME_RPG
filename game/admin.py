3b165657ccbae2a42e1b8f82cd37a4a2c93e68a7
Rafał <rawrqwe10@gmail.com>
2026-06-29 20:09:43 +0200

    dodano rejestracje item w adminie

--- game/admin.py
+++ game/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import CharacterClass, Character, Race
+from .models import CharacterClass, Character, Race, Item
 
 admin.site.register(CharacterClass)
 admin.site.register(Race)
 admin.site.register(Character)
+admin.site.register(Item)
